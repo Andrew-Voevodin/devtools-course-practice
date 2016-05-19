932fcb62dda6e21c98dc637820158fc018711117
Andrew-Voevodin <andrvoevodin@mail.ru>
2016-05-19 22:28:37 +0300

    Travis12

--- modules/tishkin-kostia-aconverter/test/test_sq_converter.cpp
+++ modules/tishkin-kostia-aconverter/test/test_sq_converter.cpp
@@ -18,7 +18,7 @@ class AreaConverterApplicationTest : public ::testing::Test {
             options.push_back("area-converter");
             for (size_t i = 0; i < args_.size(); ++i) {
                 options.push_back(args_[i].c_str());
-            }	
+            }
             const char** argv = &options.front();
             int argc = static_cast<int>(args_.size()) + 1;
             output_ = app_(argc, argv);

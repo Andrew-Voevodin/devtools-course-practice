54af329003b8f53b7b2dec14547f46e3b041113b
Andrew-Voevodin <andrvoevodin@mail.ru>
2016-05-19 22:23:41 +0300

    Travis11

--- modules/tishkin-kostia-aconverter/test/test_sq_converter.cpp
+++ modules/tishkin-kostia-aconverter/test/test_sq_converter.cpp
@@ -18,12 +18,10 @@ class AreaConverterApplicationTest : public ::testing::Test {
             options.push_back("area-converter");
             for (size_t i = 0; i < args_.size(); ++i) {
                 options.push_back(args_[i].c_str());
-            }
-			
+            }	
             const char** argv = &options.front();
             int argc = static_cast<int>(args_.size()) + 1;
             output_ = app_(argc, argv);
-
        }
             void Assert(std::string expected) {
             EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
